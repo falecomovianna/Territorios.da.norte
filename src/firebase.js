d462fe7df8c228b272646c01afb973df2a19e76b
falecomovianna <falecomovianna@gmail.com>
2026-05-20 18:35:13 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,13 @@
-// firebase.js
-// SUBSTITUA pelos seus dados do Firebase!
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJECT.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJECT.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyDcps4XnpNcwAi3vkwJka4aLq5z7OrDAe8",
+  authDomain: "territoriosdanorte.firebaseapp.com",
+  projectId: "territoriosdanorte",
+  storageBucket: "territoriosdanorte.firebasestorage.app",
+  messagingSenderId: "170623774430",
+  appId: "1:170623774430:web:d968189745bdc1c6ab1c4f"
 };
 
 const app = initializeApp(firebaseConfig);
